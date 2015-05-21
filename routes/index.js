2240b90b1209a027401216072833688bd36c74ba
paulavalientep <pau.valiente93@gmail.com>
2015-05-21 10:19:43 +0200

    Crear comentario - Autenticación y sesión

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
+var commentController = require('../controllers/comment_controller');
+var sessionController = require('../controllers/session_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -11,6 +13,11 @@ router.get('/', function(req, res) {
 //Autoload de comandos con :quizId
 
 router.param('quizId', quizController.load); //autoload :quizId
+
+// Definición de rutas de sesion
++router.get('/login', sessionController.new); // formulario login
++router.post('/login', sessionController.create); // crear sesión
++router.get('/logout', sessionController.destroy); // destruir sesión
 
 //Definición de las rutas de /quizes
 router.get('/quizes', quizController.index);
@@ -22,5 +29,8 @@ router.get('/quizes/:quizId(\\d+)/edit', quizController.edit);
 router.put('/quizes/:quizId(\\d+)', quizController.update);
 router.delete('/quizes/:quizId(\\d+)', quizController.destroy);
 
+
+router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
+router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
 
 module.exports = router;
